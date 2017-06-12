5e24eba147ef864a547b639b12f8f43957b4e8a3
jinimcoroneo <jinim1983@gmail.com>
2017-06-12 16:06:39 +0100

    fixing rubocop offenses

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,11 @@
 class OysterCard
-
   attr_reader :balance
   MAXIMUM_BALANCE = 90
 
   def initialize(balance = 0)
     @balance = balance
   end
-
+  
   def top_up(amount)
     raise "Can't top up beyond #{MAXIMUM_BALANCE} pounds" if @balance + amount > MAXIMUM_BALANCE
     @balance += amount
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,25 +1,22 @@
-require "oystercard"
-
+require 'oystercard'
 describe OysterCard do
-
-  it "expects to initialize with a balance of 0" do
+  it 'expects to initialize with a balance of 0' do
     expect(subject.balance).to eq(0)
   end
 
-  describe "#top_up" do
+  describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
-    it "tops up the balance" do
+    it 'tops up the balance' do
       subject.top_up(50)
       expect(subject.balance).to eq 50
     end
 
-    it "won't top up beyond maximum limit" do
+    it 'won\'t top up beyond maximum limit' do
       subject.top_up(1)
       expect { subject.top_up(OysterCard::MAXIMUM_BALANCE) }.to raise_error("Can't top up beyond 90 pounds")
     end
   end
-
   # describe "#above_limit?" do
   #   it { is_expected.to respond_to(:above_limit?)}
   #
@@ -32,5 +29,4 @@ describe OysterCard do
   #     expect(card).to be_above_limit
   #   end
   # end
-
 end
