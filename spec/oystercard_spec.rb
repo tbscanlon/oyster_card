6a5789c652244324ff9377596282bded1c1d9cc2
jinimcoroneo <jinim1983@gmail.com>
2017-06-12 15:59:25 +0100

    used  maximum balance constant to raise error

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,6 +13,24 @@ describe OysterCard do
       subject.top_up(50)
       expect(subject.balance).to eq 50
     end
+
+    it "won't top up beyond maximum limit" do
+      subject.top_up(1)
+      expect { subject.top_up(OysterCard::MAXIMUM_BALANCE) }.to raise_error("Can't top up beyond 90 pounds")
+    end
   end
+
+  # describe "#above_limit?" do
+  #   it { is_expected.to respond_to(:above_limit?)}
+  #
+  #   it "returns false if the card is below the limit" do
+  #     expect(subject).to_not be_above_limit
+  #   end
+  #
+  #   it "returns true if the card is above the limit" do
+  #     card = OysterCard.new(100)
+  #     expect(card).to be_above_limit
+  #   end
+  # end
 
 end
